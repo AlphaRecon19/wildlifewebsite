a9c6ede87e75cdaa53033d9e195323ae61964de3
AlphaRecon19 <doodnent@hotmail.co.uk>
2014-05-20 10:12:08 +0100

    Create merchandise.php
    new

---
+++ pages/merchandise.php
@@ -0,0 +1,29 @@
+<?php
+//======================================================================
+//   Copyright 2014 @AlphaRecon19
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//======================================================================
+include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+<div class="container bs-docs-container">
+<div class="row">
+<div class="col-md-11" role="main">
+<!-- <?php echo $merchandise_title; ?> -->
+<div class="bs-docs-section">
+<h1 class="page-header"><?php echo $merchandise_title; ?></h1>
+<?php echo $merchandise_text; ?>
+</div>
+</div>
+</div>
+</div>
